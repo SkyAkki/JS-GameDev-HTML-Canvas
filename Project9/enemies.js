2ac58782c699ee7b576bd65f948b4003e219163e
Akash Saxena <akashsaxena5555@gmail.com>
2022-08-31 19:42:47 +0530

    add spider enemy type

--- Project9/enemies.js
+++ Project9/enemies.js
@@ -48,8 +48,44 @@ export class FlyingEnemy extends Enemy{
     }
 }
 export class GroundEnemy extends Enemy{
-
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 60;
+        this.height = 87;
+        this.x = this.game.width;
+        this.y = this.game.height - this.height - this.game.groundMargin;
+        this.image = enemy_plant;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.maxFrame = 1;
+    }
 }
 export class ClimbingEnemy extends Enemy{
-
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 120;
+        this.height = 144;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.image = enemy_spider_big;
+        this.speedX = 0;
+        this.speedY = Math.random() > 0.5 ? 1 : -1;
+        this.maxFrame = 5;
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        if(this.y > this.game.height - this.height - this.game.groundMargin) this.speedY *= -1;
+        if(this.y < -this.height){
+            this.markedForDeletion = true;
+        }
+    }
+    draw(context){
+        super.draw(context);
+        context.beginPath();
+        context.moveTo(this.x + this.width * 0.5,0);
+        context.lineTo(this.x + this.width * 0.5, this.y + this.height * 0.5);
+        context.stroke();
+    }
 }
--- Project9/main.js
+++ Project9/main.js
@@ -51,6 +51,8 @@ window.addEventListener('load',function(){
             })
         }
         addEnemy(){
+            if(this.speed > 0 && Math.random() > 0.5) this.enemies.push(new GroundEnemy(this));
+            else if(this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
             this.enemies.push(new FlyingEnemy(this));
         }
     }
